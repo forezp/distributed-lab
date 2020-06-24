ba6b0377b7577ac877659579434015c28c79996d
nickelfang <124746406@qq.com>
2020-06-24 15:10:33 +0800

    boot-es

--- boot-es/src/main/java/io/github/forezp/es/EsApplication.java
+++ boot-es/src/main/java/io/github/forezp/es/EsApplication.java
@@ -2,7 +2,9 @@ package io.github.forezp.es;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class EsApplication {
 
