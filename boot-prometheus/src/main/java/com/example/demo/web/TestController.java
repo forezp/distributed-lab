61b81b6d987b3e0247b3562124f297d6c550e478
nickelfang <124746406@qq.com>
2020-07-09 17:28:05 +0800

    prometheus

---
+++ boot-prometheus/src/main/java/com/example/demo/web/TestController.java
@@ -0,0 +1,23 @@
+package com.example.demo.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    Logger logger = LoggerFactory.getLogger(TestController.class);
+
+    @GetMapping("/test")
+    public String test() {
+        logger.info("test");
+        return "ok";
+    }
+
+    @GetMapping("")
+    public String home() {
+        logger.info("home");
+        return "ok";
+    }
+}
